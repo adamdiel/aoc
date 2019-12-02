4fede4d25213f241d69bdb5c26546a67d18a050e
Adam Diel <adamdiel@github.com>
2019-12-02 15:52:42 -0800

    Day1 Complete!

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,18 +1,28 @@
 #!/usr/bin/env ruby
 
+def calculate_fuel mass
+  fuel = mass/3
+  fuel = fuel-2
+  puts fuel
+  fuel
+end
 
 def calculate_module_fuel mass
-  fuel = mass/3
-  fuel = fuel - 2
+  fuel = calculate_fuel(mass)
+  if fuel <= 0
+    0
+  else
+    fuel + calculate_module_fuel(fuel)
+  end
 end
 
 filename = ARGV[0]
 
-totalFuel = 0
+total_fuel = 0
 
 File.readlines(filename).each do |line|
   module_fuel = calculate_module_fuel(line.to_i)
-  totalFuel = totalFuel + module_fuel
+  total_fuel = total_fuel + module_fuel
 end
 
-puts totalFuel
+puts total_fuel
