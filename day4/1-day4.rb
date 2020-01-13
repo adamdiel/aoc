5e1f222fc97a61cb44e2260ff67ac46b7146181d
Adam Diel <adamdiel@github.com>
2020-01-13 13:14:53 -0700

    Part 2 is working in this file as well, line 21 to 31 was added for that. Just return if that flag is true or not for part 1

--- day4/1-day4.rb
+++ day4/1-day4.rb
@@ -6,7 +6,31 @@ def check_sequence(seq)
   number_array = seq.to_s.split('').map(&:to_i)
 
   index = 0
-  
+
+  while index < number_array.length()-1
+    if number_array[index] == number_array[index+1]
+      repeat_flag = true
+      index = index+1
+    elsif number_array[index] > number_array[index+1]
+      return false
+    else
+      index = index+1
+    end
+  end
+
+  if repeat_flag == true
+    counts = Hash.new(0)
+    number_array.each { |num| counts[num] += 1}
+    counts.each do |key, value|
+      if value == 2
+        return true
+      end
+    end
+  end
+
+  return false
+
+
 end
 
 input = ARGV[0].split("-")
@@ -16,6 +40,11 @@ high = input[1]
 
 range = low..high
 
+count = 0
 for r in range
-  check_sequence(r)
+  if check_sequence(r)
+    count = count + 1
+  end
 end
+
+puts count
