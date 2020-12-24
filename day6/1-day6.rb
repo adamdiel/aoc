a4c60420d065cdb7143fe777cde33dbeb7f2c857
Adam Diel <adamdiel@github.com>
2020-12-24 01:08:44 +0000

    day 6 done

--- day6/1-day6.rb
+++ day6/1-day6.rb
@@ -1,8 +1,20 @@
 #!/usr/bin/env ruby
 
-def question_count(custom)
-  party_count = custom.scan(/\n/).count + 1
-  
+def question_count(custom_form)
+  party_count = custom_form.scan(/\n/).count + 1
+
+  people = custom_form.split(" ")
+
+  answer_hash = Hash.new(0)
+  people.each do | answers | 
+    answers.split('').each do | answer|
+      answer_hash[answer] += 1
+    end
+  end
+
+  count = answer_hash.select{ | key, value| value == party_count}.count
+
+  return count
 end
 
 filename = ARGV[0]
@@ -14,5 +26,7 @@ customs = customs.split(/\n{2,}/)
 
 valid_count = 0
 customs.each do |custom|
-  puts question_count(custom)
+  valid_count += question_count(custom)
 end
+
+puts valid_count
